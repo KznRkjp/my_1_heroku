a220b96a16714ee49870f753c119805c24403a65
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-02-06 15:48:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def index():
   x = random()
 
   return {
-    "date": f"{now.year}-{now.month}-{now.day}",
+    "date": f"{now.year}-{now.month}-",
     "predictions": horoscope.generate_prophecies(5,3),
     "special_date": x > 0.5,
     "x": x,
